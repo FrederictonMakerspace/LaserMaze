64ffff4dc2d41d67613345217cbf72ac429ea815
Rick McCaskill <rick.mccaskill@gmail.com>
2018-07-16 08:13:57 -0300

    lots of changes
    lots of changes from @theaterman72  including detection of the pressure switch

--- Laser_Maze_2018_Sketch_FMS/Laser_Maze_2018_Sketch_FMS.ino
+++ Laser_Maze_2018_Sketch_FMS/Laser_Maze_2018_Sketch_FMS.ino
@@ -2,16 +2,17 @@
 /* Read Photo resisor values and send them to a PC (procssing) to monitor lasers in a laser maze)
    More info and circuit schematic: https://github.com/FrederictonMakerspace/LaserMaze
    Dev: Rick McCaskill, Fredericton Makerspace 2018
+   Dev: James Gaudet, Fredericton Makerspace 2018
 */
 const int ledPin=13;       // Led pin at Arduino pin 13
 const int laserPin0=8;       // Laser (5mw) connected to pin at Arduino pin 8
 const int laserPin1=9;       // Laser (5mw) connected to pin at Arduino pin 9
-const int laserPin2=10;       // Laser (5mw) connected to pin at Arduino pin 10
-const int laserPin3=11;       // Laser (5mw) connected to pin at Arduino pin 11
+const int laserPin2=10;      // Laser (5mw) connected to pin at Arduino pin 10
+const int laserPin3=11;      // Laser (5mw) connected to pin at Arduino pin 11
+const int pressureSwitch=7;  // Controls the 'treasure' detection. The treasure holds the switch closed. 
 int SystemStatus=4;
 
 const int resistorPins[] = {A0, A1, A2, A3}; //Photoresistor at Arduino analog pin A0
-//const int laserPins[] = {8, 9, 10, 11}; //Photoresistor at Arduino analog pin A0
 int resistorCount = 4;
 int laserCount = 4;
 
@@ -20,6 +21,8 @@ int resistorValues[] = {0, 0, 0, 0};  // Store value from photoresistor (0-100is
 
 void setup()
 {
+  pinMode(pressureSwitch, INPUT);
+  digitalWrite(pressureSwitch, HIGH);
   pinMode(ledPin, OUTPUT);  // Set ledPin - 13 pin as an output
   pinMode(laserPin0, OUTPUT);  // Set laserPin - 8 pin as an output
   pinMode(laserPin1, OUTPUT);  // Set laserPin - 9 pin as an output
@@ -33,16 +36,8 @@ void setup()
     pinMode(pin, INPUT);// Resistor input 
   }
 
-     randomSeed (analogRead (4));    // randomize
+  randomSeed (analogRead (4));    // randomize
     
-/*    for (int i=0; i<laserCount; i++)
-  {
-    int pin = laserPins[i];
-    pinMode(pin, OUTPUT);// Resistor input 
-  }
-*/
-
-
   digitalWrite(ledPin, LOW); //Turn led off
   digitalWrite(laserPin0, LOW); //Turn led off
   digitalWrite(laserPin1, LOW); //Turn led off
@@ -54,8 +49,7 @@ void setup()
 }
 
 void loop()
-{
-  
+{  
   //Read all resistors and store the valuye
   for (int i=0; i<resistorCount; i++)
   {
@@ -64,7 +58,19 @@ void loop()
     //Store the value in the array
     resistorValues[i] = analogRead(resistorToRead);
   }
-
+  
+  //Check if the treasure has been stolen
+  int switchStatus=digitalRead(pressureSwitch);
+  if (switchStatus == HIGH)
+  {
+    //Yep! Treasure stolen!
+    SystemStatus=6;
+  }
+  else
+  {
+    //Nope! Treasure is in place
+    SystemStatus=0;  
+  }
   String serialMessage = (String)SystemStatus + ":" + (String)resistorValues[0] + "," + (String)resistorValues[1] + "," + (String)resistorValues[2] + ","  + (String)resistorValues[3];
   Serial.println(serialMessage);
 
@@ -73,9 +79,10 @@ void loop()
    if (Serial.available()) 
    { 
       // If data is available to read,
-   int  val = Serial.read(); // read it and store it in val from the processing app on the attached PC
+      int  val = Serial.read(); // read it and store it in val from the processing app on the attached PC
      
-          switch (val) {
+       switch (val)
+       {
           case '0': //game running
             SystemStatus=0;
             laserOn();
@@ -87,43 +94,21 @@ void loop()
           break;
           
           case '2': // case 2 System reset
-        //reset system
-           systemReset();
+           //reset system
+            SystemStatus=2;
+            systemReset();
           break;
 
           case '3':
+            SystemStatus=3;
             attractMode();
           break;
-          }
+       }
    }
-
-
-    //ALARM
-/*    if (val == '1') 
-    {
-      digitalWrite(ledPin, LOW); //Turn led off
-      digitalWrite(laserPin, LOW); //Turn laser off
-    } 
-    else
-    {
-     digitalWrite(ledPin, HIGH); //Turn led on
-     digitalWrite(laserPin, HIGH); //Turn laser on
-     }
-*/
-  
-    
-  //Wrap for compatibility with the laser processing sketch (comma separated values)
-//  String serialMessage = "0:" + (String)resistorValues[0] + ",0:" + (String)resistorValues[1] + ",0:" + (String)resistorValues[2] + ",0:"  + (String)resistorValues[3];
-
-  //Send to PC
-  //Serial.println(serialMessage);
-
-  //delay(50); //Small delay
-
 }
 
-
 void systemReset() {
+    //flash lasers randomly on and off
     laserOn();
     delay(200);
       digitalWrite(ledPin, LOW); //Turn led on
@@ -140,12 +125,7 @@ void systemReset() {
       digitalWrite(laserPin2, HIGH); //Turn laser on
       delay(200);
       digitalWrite(laserPin3, HIGH); //Turn laser on
-  // turn alarm off
-  // turn lasers on
-    //flash lasers randomly on and off
   // then let the PC know I'm reset
-  // reset PC so timer is 0, and start button is available.
-   // digitalWrite(alarmPin, High);
     String serialMessage = "1:"; //need to verify what to send to PC
 }
 
@@ -166,39 +146,34 @@ void laserOff() {
       triggerAlarm();
 }
 
-void triggerAlarm() {
-  // digitalWrite(alarmPin, High);
-
-}
-
+/*
+Do something that looks cool while the gsme isn't being played
+*/
 void attractMode() {
+    digitalWrite(laserPin0, LOW);
+    digitalWrite(laserPin1, LOW);
+    digitalWrite(laserPin2, LOW);
+    digitalWrite(laserPin3, LOW);
+    for (int x = 800; x > 0; x-=200 ) {
+  //    println(x);
+  digitalWrite(laserPin0, HIGH);
+  delay(x);
   digitalWrite(laserPin0, LOW);
+  digitalWrite(laserPin1, HIGH);
+  delay(x);
   digitalWrite(laserPin1, LOW);
+  digitalWrite(laserPin2, HIGH);
+  delay(x);
   digitalWrite(laserPin2, LOW);
+  digitalWrite(laserPin3, HIGH);
+  delay(x);
   digitalWrite(laserPin3, LOW);
-  for (int x = 800; x > 0; x-=200 ) {
-//    println(x);
-digitalWrite(laserPin0, HIGH);
-delay(x);
-digitalWrite(laserPin0, LOW);
-digitalWrite(laserPin1, HIGH);
-delay(x);
-digitalWrite(laserPin1, LOW);
-digitalWrite(laserPin2, HIGH);
-delay(x);
-digitalWrite(laserPin2, LOW);
-digitalWrite(laserPin3, HIGH);
-delay(x);
-digitalWrite(laserPin3, LOW);
-digitalWrite(laserPin2, HIGH);
-delay(x);
-digitalWrite(laserPin2, LOW);
-digitalWrite(laserPin1, HIGH);
-delay(x);
-digitalWrite(laserPin1, LOW);
-
-
+  digitalWrite(laserPin2, HIGH);
+  delay(x);
+  digitalWrite(laserPin2, LOW);
+  digitalWrite(laserPin1, HIGH);
+  delay(x);
+  digitalWrite(laserPin1, LOW);
   }
-
 }
 
